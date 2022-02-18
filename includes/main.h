01dc09c06f92c2d0b1349fa24fab118812c9c0de
Mauro ABIDAL CARRER <maabidal@e2r4p12.clusters.42paris.fr>
2022-02-18 15:17:46 +0100

    define PROMPT

--- includes/main.h
+++ includes/main.h
@@ -6,7 +6,7 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:58:10 by maabidal          #+#    #+#             */
-/*   Updated: 2022/02/18 15:07:53 by maabidal         ###   ########.fr       */
+/*   Updated: 2022/02/18 15:14:24 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 # include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+# ifndef PROMPT
+#  define PROMPT "minishell: "
+# endif
 
 int	get_env_var(char *key, char **dst, char **env);
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:47:35 by maabidal          #+#    #+#             */
-/*   Updated: 2022/02/18 15:02:11 by maabidal         ###   ########.fr       */
+/*   Updated: 2022/02/18 15:17:27 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	main(int ac, char **av, char **env)
 {
 	char	*line;
 
-	line = readline("salut: ");
+	line = readline(PROMPT);
 	while (line != NULL)
 	{
 		printf("%s\n", line);
 		free(line);
-		line = readline("salut: ");
+		line = readline(PROMPT);
 	}
 }
