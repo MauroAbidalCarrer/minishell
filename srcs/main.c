9d7091f6495c39f440908ac1f38c163b4d3cc2da
Jeremy Maia <jmaia@student.42.fr>
2022-02-21 12:30:52 +0100

    Remove env variable

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:47:35 by maabidal          #+#    #+#             */
-/*   Updated: 2022/02/21 12:21:41 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/02/21 12:30:33 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,12 @@ void	exec(void)
 {
 }
 
-int	main(int ac, char **av, char **env)
+int	main(int ac, char **av)
 {
 	char	*line;
 
 	(void) ac;
 	(void) av;
-	(void) env;
 	line = readline(PROMPT);
 	while (line != NULL)
 	{
