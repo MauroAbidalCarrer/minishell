d3cf5b5e1f994769d76d0040ffeea38207fbdf99
Jeremy Maia <jmaia@student.42.fr>
2022-02-22 15:47:18 +0100

    Add builtins header file

---
+++ includes/builtins.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 15:43:27 by jmaia             #+#    #+#             */
+/*   Updated: 2022/02/22 15:47:07 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTINS_H
+# define BUILTINS_H
+
+int	ms_echo(int ac, char **av);
+int	ms_cd(int ac, char **av);
+int	ms_pwd(int ac, char **av);
+int	ms_export(int ac, char **av);
+int	ms_unset(int ac, char **av);
+int	ms_env(int ac, char **av);
+int	ms_exit(int ac, char **av);
+
+#endif
