f047c20c6e93990ce878a64cf8adf735c3bfaf21
Jeremy Maia <jmaia@student.42.fr>
2022-02-23 16:02:58 +0100

    Do not write multiple errors on error

--- srcs/builtins/ms_exit.c
+++ srcs/builtins/ms_exit.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 21:48:39 by jmaia             #+#    #+#             */
-/*   Updated: 2022/02/22 22:57:00 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/02/23 16:03:56 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,26 @@
 
 int	ms_exit(int ac, char **av)
 {
+	int	err;
+
 	if (ac == 1)
 	{
 		printf("ALERTE CECI NE DOIT JAMAIS S'AFFICHER,GO ETRE SANS GLOBALES\n");
 		return (1);
 	}
-	ft_write(2, "exit\n", 5);
+	err = (ft_write(2, "exit\n", 5) == -1);
 	if (!ft_isnbr(av[1]))
 	{
-		ft_write(2, av[0], ft_strlen(av[0]));
-		ft_write(2, ": exit: ", 8);
-		ft_write(2, av[1], ft_strlen(av[1]));
-		ft_write(2, ": numeric argument required\n", 28);
+		err = (ft_write(2, av[0], ft_strlen(av[0])) == -1);
+		err = (ft_write(2, ": exit: ", 8) == -1);
+		err = (ft_write(2, av[1], ft_strlen(av[1])) == -1);
+		err = (ft_write(2, ": numeric argument required\n", 28) == -1);
 		ft_exit(2);
 	}
 	if (ac > 2)
 	{
-		ft_write(2, av[0], ft_strlen(av[0]));
-		ft_write(2, ": exit: too many arguments\n", 27);
+		err = (ft_write(2, av[0], ft_strlen(av[0])) == -1);
+		err = (ft_write(2, ": exit: too many arguments\n", 27) == -1);
 		return (1);
 	}
 	ft_exit(ft_atoi(av[1]));
