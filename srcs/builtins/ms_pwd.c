cf1c3747e7ca47858f762b6685eb6df5fcfa3fd5
Jeremy Maia <jmaia@student.42.fr>
2022-02-23 15:47:56 +0100

    Do not write multiple error on error

--- srcs/builtins/ms_pwd.c
+++ srcs/builtins/ms_pwd.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 15:19:42 by jmaia             #+#    #+#             */
-/*   Updated: 2022/02/23 15:40:25 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/02/23 15:46:25 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,20 @@ int	ms_pwd(int ac, char **av)
 {
 	char	*pwd;
 	char	*err_msg;
+	int		err;
 
 	(void) ac;
 	pwd = getcwd(0, 0);
 	if (!pwd)
 	{
 		err_msg = strerror(errno);
-		ft_write(2, av[0], ft_strlen(av[0]));
-		ft_write(2, ": ", 2);
-		ft_write(2, err_msg, ft_strlen(err_msg));
+		err = ft_write(2, av[0], ft_strlen(av[0])) == -1;
+		err = (!err && ft_write(2, ": ", 2) == -1);
+		err = (!err && ft_write(2, err_msg, ft_strlen(err_msg)) == -1);
 		return (1);
 	}
-	ft_write(1, pwd, ft_strlen(pwd));
-	ft_write(1, "\n", 1);
+	err = ft_write(1, pwd, ft_strlen(pwd)) == -1;
+	err = (!err && ft_write(1, "\n", 1) == -1);
 	free(pwd);
 	return (0);
 }
