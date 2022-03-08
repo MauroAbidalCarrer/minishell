a42dd3f9408bafd05c24cae0c71bedc2e793c1ba
Jeremy Maia <jmaia@student.42.fr>
2022-03-08 14:39:42 +0100

    Revert main.c changes

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:47:35 by maabidal          #+#    #+#             */
-/*   Updated: 2022/03/04 15:56:18 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/02/22 18:50:20 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,17 @@ void	exec(void)
 {
 }
 
-int	ms_export(int ac, char **av, char ***env);
-
-int	main(int ac, char **av, char **env)
+int	main(void)
 {
-//	char	*line;
+	char	*line;
 
 	init_signal_handling();
-//	line = readline(PROMPT);
-//	while (line != NULL)
-//	{
-//		printf("%s\n", line);
-//		free(line);
-//		line = readline(PROMPT);
-//	}
-	ms_export(ac, av, &env);
-	printf("------------------------------\n");
-	ms_export(1, av, &env);
+	line = readline(PROMPT);
+	while (line != NULL)
+	{
+		printf("%s\n", line);
+		free(line);
+		line = readline(PROMPT);
+	}
 	write(1, "exit\n", 5);
 }
