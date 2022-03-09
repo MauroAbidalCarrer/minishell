a4cdbb704ec03b9082bffa98e6ef40eb354e2ba8
Jeremy Maia <jmaia@student.42.fr>
2022-03-09 18:24:42 +0100

    WIP wild_it

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:47:35 by maabidal          #+#    #+#             */
-/*   Updated: 2022/02/23 14:41:33 by maabidal         ###   ########.fr       */
+/*   Updated: 2022/03/08 15:57:30 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,25 @@ void	exec(char *line)
 	printf("%s\n", line);
 }
 
+int	do_match(char const *str, char const *pattern);
+
 //line doit etre free avec free et non pas avec ft_free
 int	main(int ac, char **av)
 {
-	char	*line;
+//	char	*line;
 
 	(void)ac;
-	g_exe_name = ft_strjoin(av[0], ": ");
-	init_signal_handling();
-	line = readline(g_exe_name);
-	while (line != NULL)
-	{
-		if (should_exe_list(line))
-			exec(line);
-		free(line);
-		line = readline(g_exe_name);
-	}
-	write(1, "exit\n", 5);
+	printf("%d\n", do_match(av[1], av[2]));
+//	g_exe_name = ft_strjoin(av[0], ": ");
+//	init_signal_handling();
+//	line = readline(g_exe_name);
+//	while (line != NULL)
+//	{
+//		if (should_exe_list(line))
+//			exec(line);
+//		free(line);
+//		line = readline(g_exe_name);
+//	}
+//	write(1, "exit\n", 5);
 	ft_exit(0);
 }
--- srcs/wildcard/wild_it.c
+++ srcs/wildcard/wild_it.c
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   wildcard.h                                         :+:      :+:    :+:   */
+/*   wild_it.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/08 16:27:45 by jmaia             #+#    #+#             */
-/*   Updated: 2022/03/09 17:40:34 by jmaia            ###   ########.fr       */
+/*   Created: 2022/03/09 17:38:36 by jmaia             #+#    #+#             */
+/*   Updated: 2022/03/09 17:40:12 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WILDCARD_H
-# define WILDCARD_H
+#include "wildcard.h"
 
-int		do_match(char const *str, char const *pattern);
-char	*wild_it(char const *pattern);
-
-#endif
+char	*wild_it(char const *pattern)
+{
+	
+}
