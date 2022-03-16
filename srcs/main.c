762392b65ab8e3314cd46159cd325e9a98cad655
Mauro ABIDAL CARRER <mauroabidal@yahoo.fr>
2022-03-16 17:39:55 +0100

    oui

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:47:35 by maabidal          #+#    #+#             */
-/*   Updated: 2022/03/16 17:30:41 by maabidal         ###   ########.fr       */
+/*   Updated: 2022/03/16 17:38:58 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ char	*g_exe_name;
 int	main(int ac, char **av, char **env)
 {
 	char	*line;
+	int	exit_status;
 
+	exit_status = 0;
 	(void)ac;
 	if (strrchr(av[0], '/'))
 		av[0] = strrchr(av[0], '/') + 1;
@@ -30,7 +32,7 @@ int	main(int ac, char **av, char **env)
 	while (line != NULL)
 	{
 		if (should_exe_list(line))
-			exe_list(line, 0, env);
+			exit_status = exe_list(line, 0, env);
 		free(line);
 		line = readline(g_exe_name);
 	}
