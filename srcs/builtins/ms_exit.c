359c8bd8ee8e8f3649ebce4d0101a9c0239c4b63
Jeremy Maia <jmaia@student.42.fr>
2022-04-04 17:21:59 +0200

    Fix messages in exit

--- srcs/builtins/ms_exit.c
+++ srcs/builtins/ms_exit.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 21:48:39 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/04 12:19:17 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/04 17:21:18 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 
 int	ms_exit(int ac, char **av, t_env env)
 {
-	int	err;
+	char	*err_msg;
 
 	(void) env;
 	if (ac == 1)
 		ft_exit(env.exit_status);
 	if (isatty(0))
-		err = (ft_write(2, "exit\n", 5) == -1);
+		ft_write(2, "exit\n", 5);
 	if (!ft_isnbr(av[1]))
 	{
-		err = (!err && ft_write(2, av[0], ft_strlen(av[0])) == -1);
-		err = (!err && ft_write(2, ": exit: ", 8) == -1);
-		err = (!err && ft_write(2, av[1], ft_strlen(av[1])) == -1);
-		err = (!err && ft_write(2, ": numeric argument required\n", 28) == -1);
+		err_msg = ft_malloc(sizeof(char) * (28 + ft_strlen(av[1])));
+		ft_memcpy(err_msg, av[1], ft_strlen(av[1]));
+		ft_memcpy(err_msg + ft_strlen(av[1]),
+			": numeric argument required", 27);
+		err_msg[27 + ft_strlen(av[1])] = 0;
+		write_error(NULL, "exit", err_msg);
+		ft_free(err_msg);
 		ft_exit(2);
 	}
 	if (ac > 2)
 	{
-		err = (!err && ft_write(2, av[0], ft_strlen(av[0])) == -1);
-		err = (!err && ft_write(2, ": exit: too many arguments\n", 27) == -1);
+		write_error(NULL, "exit", "too many arguments");
 		return (1);
 	}
 	ft_exit(ft_atoi(av[1]));
